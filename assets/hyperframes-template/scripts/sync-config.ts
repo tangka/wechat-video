6e2a19a83dfe12d576463cd1b46a22f5e3e76b6b
tangka <mamaohuatangka@gmail.com>
2026-06-28 17:10:14 +0800

    Sync HyperFrames duration from config

--- assets/hyperframes-template/scripts/sync-config.ts
+++ assets/hyperframes-template/scripts/sync-config.ts
@@ -8,6 +8,16 @@ async function main() {
   if (config.duration.width !== 1080 || config.duration.height !== 1440) {
     throw new Error("Template currently expects native 1080x1440. Edit index.html intentionally for other sizes.");
   }
+  const indexPath = path.join(root, "index.html");
+  const indexHtml = await fs.readFile(indexPath, "utf8");
+  if (!/<div id="root"[^>]*data-duration="[^"]+"[^>]*data-width="[^"]+"[^>]*data-height="[^"]+"/.test(indexHtml)) {
+    throw new Error("Could not find root data-duration/data-width/data-height in index.html");
+  }
+  const syncedIndexHtml = indexHtml
+    .replace(/(<div id="root"[^>]*data-duration=")[^"]+(")/, `$1${config.duration.targetSeconds}$2`)
+    .replace(/(<div id="root"[^>]*data-width=")[^"]+(")/, `$1${config.duration.width}$2`)
+    .replace(/(<div id="root"[^>]*data-height=")[^"]+(")/, `$1${config.duration.height}$2`);
+  await fs.writeFile(indexPath, syncedIndexHtml, "utf8");
   await fs.writeFile(
     path.join(root, "video.config.js"),
     `window.WWV_CONFIG = ${JSON.stringify(config, null, 2)};\n`,
